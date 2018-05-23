cf7995dfc8a1cd29666297d7ecae77b53170b807
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-23 12:43:15 +0200

    Update scripts.js content

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = prompt('Podaj długość boku (cm)'),
 	h = prompt('Podaj wysokość (cm)'),
 	triangleArea = a * h / 2;
-alert('Pole trójkąta o podstawie równej a: ' + a + 'cm i wysokości równej h: ' + h + 'cm jest równe: ' + triangleArea +'cm^2');
+alert('Pole trójkąta o podstawie równej a: ' + a + 'cm i wysokości równej h: ' + h + 'cm jest równe: ' + triangleArea + 'cm^2');
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
